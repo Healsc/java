df638f8fc8618372ea255a718b7407797ec104d3
sunchuang <873338654@qq.com>
2019-02-03 19:30:15 +0800

    study

---
+++ 方法/src/Class.java
@@ -0,0 +1,10 @@
+public class Class {
+    public static void main(String[] args){
+        getmenu();
+    }
+
+    public static void getmenu(){
+        //方法名，见名知意，首字母大写
+        System.out.println("hello");
+    }
+}
---
+++ 方法/src/Class1.java
@@ -0,0 +1,9 @@
+public class Class1 {
+    public static void main(String[] args){
+        getMenu();
+    }
+    public static int getMenu(){
+        System.out.println("hello");
+        return 1;
+    }
+}
