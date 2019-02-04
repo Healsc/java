d02063b7ab4b4bdd0c4f5ba651603d4486fcabf1
sunchuang <873338654@qq.com>
2019-02-04 21:34:45 +0800

    study

--- 方法/src/Class15.java
+++ 方法/src/Class15.java
@@ -1,15 +1,12 @@
-/**
- * 直接插入排序
- * */
 import java.util.Arrays;
-public class Class14 {
+public class Class15 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,0,9,8,7,6,5,4};
+        int[] arr = {1,2,3,4,0,9,8,7,5,6};
         int len = arr.length;
         for(int i = 1;i < len;i++){
             int temp = arr[i];
             int j;
-            for(j = i -1;j >= 0;j--){
+            for(j = i - 1;j >= 0;j--){
                 if(arr[j] > temp){
                     arr[j+1] = arr[j];
                 }else{
