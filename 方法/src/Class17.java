b502d09a7657c3aaba7126a1d44c562cae5583ee
sunchuang <873338654@qq.com>
2019-02-05 20:25:57 +0800

    study

---
+++ 方法/src/Class17.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Class17 {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,0,9,8,7,6,5,4};
+        int len = arr.length;
+        for(int i = 0;i < len;i++){
+            for(int j = 0;j < len - i - 1;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
