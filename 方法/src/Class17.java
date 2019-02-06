9e57f6f5c5e666dcdb72dd66bea2f370b9dd40fb
sunchuang <873338654@qq.com>
2019-02-06 20:31:14 +0800

    study

--- 方法/src/Class17.java
+++ 方法/src/Class17.java
@@ -11,7 +11,7 @@ public class Class17 {
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
-            }
+            } 
         }
         System.out.println(Arrays.toString(arr));
     }
