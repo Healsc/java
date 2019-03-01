8779bafb614d5d9dd79ad7ffd7b9b67e4e334058
healsc <873338654@qq.com>
2019-03-01 10:23:17 +0800

    study

--- guigu/src/TwoSum.java
+++ guigu/src/TwoSum.java
@@ -8,5 +8,34 @@ public class TwoSum {
             arr[i] = input.nextInt();
         }
         System.out.println(Arrays.toString(arr));
+        int target = input.nextInt();
+        System.out.println("target="+target);
+        int[] result = twoSum(arr,target);
+//        for(int i = 0;i < result.length;i++){
+//            System.out.println(result[i]);
+//        }
+        System.out.println(Arrays.toString(twoSum(arr,target)));
+    }
+    public static int[] twoSum(int[] nums,int target){
+        int[] result = new int[2];
+        if(nums.length < 2){
+            return result;
+        }
+        Arrays.sort(nums);
+        //System.out.println(Arrays.toString(nums));
+        int first = 0;
+        int second = nums.length - 1;
+        while(first < second){
+            if(nums[first] + nums[second] == target){
+                result[0] = nums[first];
+                result[1] = nums[second];
+                return result;
+            }if(nums[first] + nums[second] > target){
+                second--;
+            }if(nums[first] + nums[second] < target){
+                first++;
+            }
+        }
+        return result;
     }
 }
