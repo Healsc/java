0d655077d024ae4a333864bd8ef47f9b69022a43
healsc <873338654@qq.com>
2019-03-07 20:57:00 +0800

    study

---
+++ Study/src/Test5.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+public class Test5 {
+    public static void main(String[] args){
+        List list1 = new LinkedList();
+        LinkedList list2 = new LinkedList();
+        list1.add(1);
+        list2.size();
+        System.out.println(list1);
+        System.out.println(list2.size());
+    }
+}
