5f524a79069fc1eb317f0c092fc797f2b9262654
healsc <873338654@qq.com>
2019-03-08 18:20:27 +0800

    study

--- Study/src/Test6.java
+++ Study/src/Test6.java
@@ -8,7 +8,7 @@ public class Test6 {
         for(int i = 0;i < list1.size();i++){
             System.out.println(list1.get(i));
         }
-        System.out.println("--");
+        System.out.println("-----");
         System.out.println(list1.get(3));
     }
 }
